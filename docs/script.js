69830312306c029589afa5937aeb876b3de764c4
ceq5032 <125389208+ceq5032@users.noreply.github.com>
2023-12-12 19:34:04 -0500

    added a back to top button

--- docs/script.js
+++ docs/script.js
@@ -10,3 +10,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+var mybutton = document.getElementById("myBtn");
+var scrollDelay = 500; 
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        
+        setTimeout(function () {
+            mybutton.style.display = "block";
+        }, scrollDelay);
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+    mybutton.style.display = "none"; 
+}
